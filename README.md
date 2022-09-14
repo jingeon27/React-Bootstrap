b9f1eacc59c05374f6f02c954101352336b186c5
jingeon27 <jingeon27@gmail.com>
2022-09-14 11:09:22 +0900

    ✌  initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# React-Bootstrap 기능 정리
+
+- 예제에 나온대로 하면 안되고
+  > import "bootstrap/dist/css/bootstrap.min.css";
+  > 를 위에 붙이면 css도 정상 작동한다.
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import CardExample from "./pages/Example";
+
+function App() {
+  return (
+    <>
+      <CardExample />
+    </>
+  );
+}
+
+export default App;
